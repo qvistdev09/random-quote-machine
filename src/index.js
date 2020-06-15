64cc50fedd5e2ddbb7e384ce945e9aeb515ae64f
Qvistsson <63651194+qvistsson@users.noreply.github.com>
2020-06-15 20:07:55 +0200

    Text edit, move css into single file

--- src/index.js
+++ src/index.js
@@ -3,25 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Changing again! Changes are reflected. Cool!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <h1 className="text-center">This is the start of my quote machine.</h1>
     </div>
   );
 }
