2e5502d2c97f7ae1307356480925a84a946691f3
Qvistsson <63651194+qvistsson@users.noreply.github.com>
2020-06-15 19:22:23 +0200

    Updates

---
+++ precache-manifest.f144f5e28b8621783fae3dbeb32784f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2074b4abb3b5753f99dfc4d16300a69b",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "65c80132b1e77cacbfa4",
+    "url": "/random-quote-machine/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3404c843741f31076b48",
+    "url": "/random-quote-machine/static/js/2.1b90cb8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.1b90cb8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c80132b1e77cacbfa4",
+    "url": "/random-quote-machine/static/js/main.aa65a7da.chunk.js"
+  },
+  {
+    "revision": "2cec414998c69e0a692f",
+    "url": "/random-quote-machine/static/js/runtime-main.5e7c1c70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random-quote-machine/static/media/logo.5d5d9eef.svg"
+  }
+]);
